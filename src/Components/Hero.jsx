35f43bf0ce06abcb27672b4526df8cfbc164c8ad
BHANU PRATAP SINGH <70773520+bhanu9672@users.noreply.github.com>
2023-11-06 12:25:07 +0530

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Hero = () => {
     return (
         <>
-            <div className="relative overflow-hidden bg-white my-10">
+            <div className="relative overflow-hidden bg-white my-2">
                 <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
                     <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
                         <div className="sm:max-w-lg">
