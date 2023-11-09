17c71e1ad2e0711272165e10288e0cc7152482c5
BHANU PRATAP SINGH <70773520+bhanu9672@users.noreply.github.com>
2023-11-09 12:58:06 +0530

    Update Profile.js

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -17,9 +17,9 @@ const Profile = () => {
         <Tabs>
             <div>
                 <div className="mx-auto grid grid-cols-12 gap-4 bg-zinc-50 p-1">
-                    <div className="col-span-12 rounded-lg border border-gray-400 bg-gray-200 p-10 sm:col-span-3">
+                    <div className="col-span-12 rounded-lg border border-gray-400 bg-gray-200 p-10 sm:col-span-3 flex items-start">
                         {/* Sidebar */}
-                        <div className="w-64 text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                        <div className="w-72 text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                             <TabList>
                                 <Tab
                                     className="relative inline-flex items-center w-full px-4 py-4 text-sm font-medium border-b border-gray-200 rounded-t-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
