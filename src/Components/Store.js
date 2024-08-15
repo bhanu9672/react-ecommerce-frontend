c9fc4dfc204a2c1cc60b44b7241475007b7b2240
BHANU PRATAP SINGH <70773520+bhanu9672@users.noreply.github.com>
2024-08-15 18:44:35 +0530

    Update Store.js

--- src/Components/Store.js
+++ src/Components/Store.js
@@ -85,7 +85,7 @@ const Store = () => {
 
 	const ProductsList = products.length > 0 && products.map(function (data) {
 		if (data.img) {
-			const blob = new Blob([Int8Array.from(data.img.data.data)], { type: data.img.contentType });
+	//		const blob = new Blob([Int8Array.from(data.img.data.data)], { type: data.img.contentType });
 	//		var image = window.URL.createObjectURL(blob);
 var image = `data:${data.img.contentType};bade64,${(data.img.data.toString('base64'))}`;
 		}
